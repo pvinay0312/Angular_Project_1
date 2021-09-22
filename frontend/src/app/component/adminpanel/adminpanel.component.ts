74c237b0f9744a1d8f16a177f5dec2eaa7e0fa15
Vinay Patel <patelvinay41998@gmail.com>
2021-09-22 16:51:03 -0400

    user and admin page is working

--- frontend/src/app/component/adminpanel/adminpanel.component.ts
+++ frontend/src/app/component/adminpanel/adminpanel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminpanel',
@@ -7,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminpanelComponent implements OnInit {
   userDisplayName: String | null = '';
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
     this.userDisplayName = localStorage.getItem('loggedUser');
+  }
+  addProduct() {
+    this.router.navigate(["/add"]);
+  }
+
+  deleteProduct() {
+    this.router.navigate(["/delete"]);
+  }
+
+  updateProduct() {
+    this.router.navigate(["/update"]);
+  }
+
+  viewProduct() {
+    this.router.navigate(["/view"]);
+  }
+
+  logout() {
+    this.router.navigate(["/adminSignIn"]);
   }
 
 }
